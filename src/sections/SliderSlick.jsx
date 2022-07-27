43bc05e9ed5a51644616e8c3d72c50e14c5d65c4
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-28 03:19:08 +0600

    Setting Bug fixed

--- src/sections/SliderSlick.jsx
+++ src/sections/SliderSlick.jsx
@@ -5,7 +5,13 @@ import Slider from "react-slick";
 
 export default function SliderSlick() {
   
-
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+};
     
   return (
     <>
